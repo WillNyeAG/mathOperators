01b3c7c86a986330025f17fb8b5b75856cee5105
William Antonio-Gutierrez <antoniogwilliam067@gmail.com>
2019-09-24 15:19:24 -0500

    Update mathOperations.py

--- mathOperations.py
+++ mathOperations.py
@@ -4,3 +4,20 @@ Created on Sep 24, 2019
 @author: wilib
 '''
 print("Hello World")
+
+#numApples
+juan = 5
+michael = 2
+totalApples = juan + michael
+print(totalApples)
+
+#metersRun
+g = 75.67
+h = 32
+
+print(g-h)
+
+#classroomStudents
+m = 80
+n = 5
+print(m/n)

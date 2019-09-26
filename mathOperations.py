16b064a95d02e5e341e3f735e0066b7334381263
William Antonio-Gutierrez <antoniogwilliam067@gmail.com>
2019-09-25 22:26:54 -0500

    Update mathOperations.py

--- mathOperations.py
+++ mathOperations.py
@@ -5,23 +5,25 @@ Created on Sep 24, 2019
 '''
 print("Hello World")
 
-#numApples
+#numApples Addition
 juan = 5
 michael = 2
 totalApples = juan + michael
 print(totalApples)
 
-#metersRun
+#metersRun Subtraction
 g = 75.67
 h = 32
 
 print(g-h)
 
-#numChairs
+#numChairs Multiplication and concatenation
 rooms = 5
 studentsPerClass = 30
 print("The number of chairs in the school is " + str(rooms*studentsPerClass))
-#classroomStudents
+
+#classroomStudents Division
 m = 80
 n = 5
 print(m/n)
+print("World"*3)
